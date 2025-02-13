45f7f9cbac9c95f3ec5b09cbdb2dff5ad2ca72e9
Keiichiro Ono <keiono@gmail.com>
2025-02-13 00:48:17 -0800

    shadcn/ui added

--- src/app/networks/page.tsx
+++ src/app/networks/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import Layout from "@/components/Layout";
-import NetworkList from "@/components/NetworkList";
-import SearchToolbar from "@/components/SearchToolbar";
-import { Box } from "@mui/material";
+import Layout from '@/components/Layout';
+import NetworkList from '@/components/NetworkList';
+import SearchToolbar from '@/components/SearchToolbar';
+import { Box } from '@mui/material';
 
 export default function HomePage() {
   return (
     <Layout>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
         <SearchToolbar />
         <NetworkList />
       </Box>
